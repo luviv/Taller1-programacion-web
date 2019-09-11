725554da3b3929959a68bd44dca241276922a468
luviv <lavo1999@hotmail.com>
2019-09-11 01:31:46 -0500

    tercer commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function handleLoad() {
+    var button = document.querySelector('#right');
+    var buttonl = document.querySelector('#left');
+    var slider = document.querySelector('.slider');
+    var tape = slider.querySelector('.slider__tape');
+    var count = 0;
+
+    var first = tape.querySelector('.slider__img');
+    var newLast = document.createElement('img');
+    newLast.setAttribute('src', first.getAttribute('src'));
+    newLast.classList.add('slider__img');
+    tape.append(newLast);
+
+    function handleLast(){
+        console.log('cambio');
+        tape.classList.add('slider__tape--inactive');
+        tape.style.transform = 'translate(0px, 0px)';
+    }
+
+    function handleClick(event) {
+        count++;
+    
+        if(count == 1){
+            tape.classList.remove('slider__tape--inactive');
+        }
+    
+        var mov = slider.offsetWidth * -1 * count;
+        tape.style.transform = 'translate(' + mov + 'px, 0px)';
+        
+        if(count > tape.childElementCount - 2){
+            setTimeout(handleLast, 300);
+            count = 0;
+        }
+    }
+
+    function handleClick(event) {
+        count++;
+    
+        if(count == 1){
+            tape.classList.remove('slider__tape--inactive');
+        }
+    
+        var movl = slider.offsetWidth * 1 * -count;
+        tape.style.transform = 'translate(' + movl + 'px, 0px)';
+        
+        if(count > tape.childElementCount - 2){
+            setTimeout(handleLast, 300);
+            count = 0;
+        }
+    }
+
+    button.addEventListener('click', handleClick);
+    buttonl.addEventListener('click', handleClick);
+}
+
+window.addEventListener('load', handleLoad);
